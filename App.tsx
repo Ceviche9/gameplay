3f624d874e677ca78b58113b0f218796726dd56c
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-06-22 13:50:33 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { SignIn } from './src/screens/SignIn';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+
+export default function App(){
+  
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+
+  return(
+   <SignIn />
+  );
+}
