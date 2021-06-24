e11ae06b405c35c09af0be1aa9a45b252cf459fc
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-06-23 22:40:55 -0300

    style fix

--- src/components/Guild/styles.ts
+++ src/components/Guild/styles.ts
@@ -6,22 +6,22 @@ export const styles = StyleSheet.create({
     width: '100%',
     flexDirection: 'row',
     alignItems: 'center',
-    paddingHorizontal: 24,
+    paddingHorizontal: 24,    
   },
   content: {
     flex: 1,
     justifyContent: 'center',
+    marginLeft: 20
   },
   title: {
     fontFamily: theme.fonts.title700,
     color: theme.colors.heading,
     fontSize: 18,
-    marginBottom: 11,
+    marginBottom: 4,
   },
   type: {
     fontFamily: theme.fonts.text400,
     color: theme.colors.highlight,
     fontSize: 13,
-    marginBottom: 24,
   }
 });
